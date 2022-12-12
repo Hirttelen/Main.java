8450350ac2407feb4d7d380b493301ae68a5e1eb
Hirttelen <hirt.gergo@gmail.com>
2022-12-12 15:19:23 +0100

    v1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static  void  main(String[] args){
-        System.out.println("Hello world!");
+        String sor;
+        do {
+            Scanner sc=new Scanner(System.in);
+            sor=sc.nextLine();
+            switch (sor){
+                case "hello":
+                    System.out.println("Hello World");
+                break;
+                case "info":
+                    System.out.println("Alpha");
+                break;
+                case "exit":
+                    System.out.println("Kilépés...");
+                break;
+                default:
+                    System.out.println("Nincs ilyen parancs");
+            }
+        }while (!sor.equals("exit"));
 
     }
 }
